0bb85d5c62453bf1f1cc45483aab99192ee641f5
luizmathart <l.matheushartmann.m@gmail.com>
2026-05-27 16:31:25 -0300

    teste

--- erxercicio.py
+++ erxercicio.py
@@ -25,6 +25,7 @@ with open('exercicio.csv', 'r') as arquivo:
     print(f'o numero de consultas foi de {numero_de_consutas}')
     print(f'o numero de consultas foi de {valor_total_arrecadado}')
     print(f'o valor medio de atendimentos foi de {round(valor_médio_atendimentos)}')
-#teste
+
+#mudanca aqui por favor mude
     
 
